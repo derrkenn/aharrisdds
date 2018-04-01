ceedfd0b6ecc70f273a7c85879345b3e9926cf66
Derrick Kennedy <dkennedy@nexient.com>
2018-04-01 08:51:07 -0400

    add routing for primary pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutMePageComponent } from './about-me-page/about-me-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductionPageComponent } from './production-page/production-page.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'about-me', component: AboutMePageComponent},
+  {path: 'production', component: ProductionPageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMePageComponent,
+    HomePageComponent,
+    ProductionPageComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule
   ],
   providers: [],
